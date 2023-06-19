b395a151638ac8df67fbc06d964e05d9903492bc
Mustafa <mustafa.abdalla@tu-clausthal.com>
2023-06-19 19:01:09 +0200

    added combination choice

--- hitomezashi.py
+++ hitomezashi.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 
+
 WIDTH = 1280
 HEIGHT = 720
 SIZE = 40
@@ -15,6 +16,8 @@ columnOptions = [[1,0,1,1,0,1,0,0,0,0,1,1,1,1,1,1,0,1,0,0,1,0,0,1,0,1,1,0,0,1,0,
                  [1,1,0,1,0,0,1,1,0,0,0,1,0,1,0,1,1,0,1,1,0,1,1,1,1,0,1,1,0,0,1,1,0,1,0,1,0,0,1,1]]
 
 columns = columnOptions[0]
+
+count = 0
 
 win = pygame.display.set_mode((WIDTH, HEIGHT))
 running = True
@@ -74,12 +77,42 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         # creating 2 random bit streams for the rows and columns
-        elif event.type == BITCHOICE:
-            print("choice")
-            for i in range(3):
-                for j in range(3):
-                    rows = rowOptions[i]
-                    columns = columnOptions[j]
+        # elif event.type == BITCHOICE:
+        #     print("choice")
+        #     for i in range(3):
+        #         for j in range(3):
+        #             rows = rowOptions[i]
+        #             columns = columnOptions[j]
+
+    # choosing combination
+    if count == 0:
+        rows = rowOptions[0]
+        columns = columnOptions[0]
+    elif count == 1:
+        rows = rowOptions[0]
+        columns = columnOptions[1]
+    elif count == 2:
+        rows = rowOptions[0]
+        columns = columnOptions[2]
+    elif count == 3:
+        rows = rowOptions[1]
+        columns = columnOptions[0]
+    elif count == 4:
+        rows = rowOptions[1]
+        columns = columnOptions[1]
+    elif count == 5:
+        rows = rowOptions[1]
+        columns = columnOptions[2]
+    elif count == 6:
+        rows = rowOptions[2]
+        columns = columnOptions[0]
+    elif count == 7:
+        rows = rowOptions[2]
+        columns = columnOptions[1]
+    elif count == 8:
+        rows = rowOptions[2]
+        columns = columnOptions[2]
+
 
     win.fill((135, 80, 148))
     
@@ -106,3 +139,9 @@ while running:
             rectangle(win, (162, 205, 72), multiply(x, (divide(WIDTH, SIZE))), multiply(y, (divide(WIDTH, SIZE))), 3,
                       (divide(WIDTH, SIZE)))
 
+    if count > 8 :
+        count = 0
+    else :
+        count += 1
+
+    # pygame.display.update()
